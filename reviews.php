7649dc4082a94fcb4a75d7ecfaac70e683cd6d2d
samanwitha <31465370+samanwitha@users.noreply.github.com>
2021-12-17 15:50:11 -0500

    Code for Reviews, User Login, Ratings

--- reviews.php
+++ reviews.php
@@ -1,4 +1,177 @@
-// Code for Reviews
+// Code for Reviews, User Login, Ratings
+
+
+Reviews:
+
+<?php
+
+echo '
+<!DOCTYPE html>
+<html>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+h1,h2,h3,h4,h5,h6 {font-family: "Oswald"}
+body {font-family: "Open Sans"}
+</style>
+<body class="w3-light-grey">
+
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1600px">
+
+
+
+    <header class="w3-display-container w3-wide" id="home">
+    <img class="w3-image" src="p10.jpg" alt="Fashion Blog" width="1600" height="1060">
+    <div class="w3-display-right w3-padding-large">
+	
+      <h1 class="w3-text-white ">Ratings4u</h1>
+      <h1 class="w3-jumbo w3-text-white  w3-hide-small"><b>MOVIES</b></h1>
+        </div>
+  </header>
+
+      <div class="w3-container w3-white w3-margin w3-padding-large">
+        <div class="w3-center">
+         ';
+	     echo'<h3>'.$name.'</h3>
+        </div>
+
+        <div class="w3-justify">
+		<p class="w3-clear"></p>
+          <div class="w3-row w3-margin-bottom"><hr>
+		  <form class="w3-container" method="POST" action="revsuccess.php" >
+
+<label class="w3-text-black"><b>Reviewer Name</b></label>
+<input class="w3-input w3-border" type="text"  name="revr">
+ 
+<label class="w3-text-black"><b>Review</b></label>
+<textarea class="w3-input w3-border" rows="5" cols="15" type="text" name="rev"></textarea><br>
+
+<button class="w3-btn w3-black" type="submit">Submit</button>
+ 
+</form>
+		  </div>
+        </div>
+      </div>
+      <hr>
+
+    
+    </div>
+
+
+  <!-- END GRID -->
+  </div>
+
+<!-- END w3-content -->
+</div>
+
+
+
+<!-- Footer -->
+<footer class="w3-container w3-dark-grey" style="padding:32px">
+  <a href="#" class="w3-button w3-black w3-padding-large w3-margin-bottom"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+ 
+
+User login:
+
+<?php
+
+require 'connect.inc.php';
+if(isset( $_POST['submit_1'] )) {
+
+
+session_start();
+if(isset($_POST['unl'])&& isset($_POST['pwl']))
+{
+	$username=$_POST['unl'];
+	
+	$password=$_POST['pwl'];
+
+	
+	if(!empty($username)&&  !empty($password))
+	{
+		
+	
+	
+	 $con = mysqli_connect('localhost','root','');
+		 
+		 mysqli_select_db($con,'rudb');
+		 
+		 $sql = "select * from users where username='$username'";
+		 
+		 $records = mysqli_query($con,$sql);
+		
+		if($row = mysqli_fetch_array($records))
+		{
+			
+			
+			if(password_verify($password,$row['password']))
+			{
+				
+				$_SESSION['uid'] = $row['id'];
+					$_SESSION['uname'] = $row['username'];
+					
+				header('Location:r4uusercat.php');
+
+
+			}
+			else
+			{
+			 echo '<span  class="btn-bar w3-red " > <button   title="Click to close" class="w3-btn  w3-medium  w3-round-large   " onclick="this.parentElement.style.display=\'none\'"   style="width:100%" >Invalid  password </button></span>';
+
+			}
+		
+		}
+		else
+	{
+		echo' <span  class="btn-bar  w3-red" > <button   title="Click to close" class="w3-btn  w3-medium  w3-round-large   " onclick="this.parentElement.style.display=\'none\'"   style="width:100%" >Invalid username</button></span> ';
+	}
+				
+	}
+	
+	else
+	{
+		echo' <span  class="btn-bar  w3-red" > <button   title="Click to close" class="w3-btn  w3-medium  w3-round-large   " onclick="this.parentElement.style.display=\'none\'"   style="width:100%" >  Enter valid username and password</button></span> ';
+	}
+	
+}	
+
+}
+else if(isset( $_POST['submit_2'] )) {
+
+if(isset($_POST['unr'])&& isset($_POST['emr'])&& isset($_POST['pwr'])&& isset($_POST['phr']))
+{
+	$user=$_POST['unr'];
+	$email=$_POST['emr'];
+	$pass=$_POST['pwr'];
+	$phonenum=$_POST['phr'];
+	 
+	$hashedPwd = password_hash($pass,PASSWORD_BCRYPT);
+		
+		$query="INSERT INTO users VALUES('','".mysql_real_escape_string($user)."','".mysql_real_escape_string($hashedPwd)."','".mysql_real_escape_string($email)."','".mysql_real_escape_string($phonenum)."');";
+		
+		if($query_run=mysql_query($query))
+		{
+			echo '<span  class="btn-bar  w3-green" > <button   title="Click to close" class="w3-btn  w3-medium  w3-round-large   " onclick="this.parentElement.style.display=\'none\'"   style="width:100%" >        You have successfully signed-in with following details<b> <br> Username : '.$user.'<br> Email: '.$email.'<br> PhoneNumber : '.$phonenum.'</button></span>';
+				
+			
+			
+		}
+		else
+		{
+			
+			echo '<span  class="btn-bar w3-red" > <button   title="Click to close" class="w3-btn w3-medium  w3-round-large   " onclick="this.parentElement.style.display=\'none\'"   style="width:100%" > <strong> Warning! </strong>&nbsp;  Registration failed.You cannot leave any field empty or Username might have  already been used.</button></span>';
+
+
+Rating:
 
 <?php
 include ('sentiment_analyser.class.php');
