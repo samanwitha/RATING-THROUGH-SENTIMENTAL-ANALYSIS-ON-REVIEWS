8189dbd414591531a823429c8699a879e85b3488
samanwitha <31465370+samanwitha@users.noreply.github.com>
2021-12-17 15:51:15 -0500

    Create db-connection.php

---
+++ db-connection.php
@@ -0,0 +1,16 @@
+//Connection to database
+
+<?php 
+$conn_error='could not connect';
+$my_host = 'localhost';
+$my_user = 'root';
+$my_pass= '';
+$my_db='rudb';
+
+if(!mysql_connect($my_host,$my_user,$my_pass)|| !mysql_select_db($my_db))
+{
+	die($conn_error);
+	
+}
+
+?>
